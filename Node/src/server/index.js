7c73739ac0b6528f76b9f37c569fe8fa48613f0e
Javier García González <javier@garciagon.com>
2021-02-20 13:07:24 +0100

    Add prettier and eslint to lint and format the code

--- Node/src/server/index.js
+++ Node/src/server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const server = express();
-server.use(bodyParser.urlencoded({extended: false}));
+server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
 
 module.exports = server;
